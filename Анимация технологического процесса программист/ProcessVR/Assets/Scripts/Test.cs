14cb27587e28c2a1c3cf552178ec6ccfc9299107
PLayL04 <88071197+PLayL04@users.noreply.github.com>
2021-07-29 17:12:11 +0700

    первый коммит

---
+++ Анимация технологического процесса программист/ProcessVR/Assets/Scripts/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float force;
+    // Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnDown()
+    {
+        rb.AddForce(Vector3.up*force, ForceMode.Impulse);
+    }
+}
---
+++ Анимация технологического процесса программист/ProcessVR/Assets/Scripts/Waypoint Test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointTest : MonoBehaviour
+{
+    public Transform[] Points;
+    public float Speed = 0.0f, Distance = 0.0f;
+    private int _currentPoint;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        if (_currentPoint == Points.Length) 
+            _currentPoint = 0;
+        float _currentDistance = Vector3.Distance(transform.position, Points[_currentPoint].position);
+        if (_currentDistance <= Distance)
+            _currentPoint++;
+        transform.LookAt(Points[_currentPoint].position);
+        transform.position = Vector3.MoveTowards(transform.position, Points[_currentPoint].position, Speed * Time.deltaTime);
+	}
+}
